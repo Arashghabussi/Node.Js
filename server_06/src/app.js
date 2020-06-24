9173de43f8f5544a69385687ed234a4232f27a73
arashghabussi <>
2020-06-24 14:50:43 -0700

    JSON

---
+++ server_06/src/app.js
@@ -0,0 +1,73 @@
+const yargs = require('yargs');
+const chalk = require('chalk');
+const { string } = require('yargs');
+
+yargs.version('1.1.0');
+
+// To Do List
+
+// Adding:
+yargs.command({
+    command: 'add',
+    describe: 'add a new task',
+    builder: {
+        title: {
+            describe: 'include a title for the task',
+            demandOption: true,
+            type: 'string'
+        },
+        task: {
+            describe: 'define your task',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log(`task added!`);
+        console.log(`title is '${argv.title}' and the task is '${argv.task}'`);
+    }
+});
+
+//
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove an existing task',
+    handler: function () {
+        console.log('task removed');
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'read an task',
+    handler: function () {
+        console.log('task read');
+    }
+});
+
+yargs.command({
+    command: 'edit',
+    describe: 'edit an existing task',
+    handler: function () {
+        console.log('task edited');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'list of tasks',
+    handler: function () {
+        console.log('tasks');
+    }
+});
+
+yargs.command({
+    command: 'history',
+    describe: 'history of tasks',
+    handler: function () {
+        console.log('history of tasks');
+    }
+});
+
+yargs.parse();
---
+++ server_06/src/json/play.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const book = {
+    title: 'Runixtra',
+    describtion: 'From Innovation Into Creation'
+}
+
+//const bookJSON = JSON.stringify(book);
+//const parsedBook = JSON.parse(bookJSON);
+
+const bookJSON = JSON.stringify(book);
+fs.writeFileSync('book.txt', bookJSON);
+
+const bookBuffer = fs.readFileSync('book-json.json');
+const dataJSON = bookBuffer.toString();
+const data = JSON.parse(dataJSON);
+
+console.log(data);
+console.log(data.title);
