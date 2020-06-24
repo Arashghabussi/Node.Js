e976d6ff1e77222455fcd438e717febe0318d09a
arashghabussi <>
2020-06-24 11:55:53 -0700

    server

--- server_03/src/app.js
+++ server_03/src/app.js
@@ -1,33 +1,12 @@
-// Email checking
+// Email checking & Chalk
 
 const validator = require('validator');
 const chalk = require('chalk');
-const yargs = require('yargs');
-const { describe } = require('yargs');
 
 if(validator.isEmail('email@gmail.com')){
-    //console.log('');
+    console.log('Email True');
 } else{
-    //console.log('false');
+    console.log('Email False');
 }
 
-yargs.version('1.3.3');
-yargs.command({
-    command: 'add',
-    describe: 'describttion',
-    handler: function (){
-        console.log('function');
-    }
-})
-
 const connected = chalk.red.bgBlack.italic.underline('connected');
-
-//console.log(validator.isURL('google.com'));
-//console.log(connected);
-
-//console.log(process.argv);
-console.log(yargs.argv[1]);
-
-//if(yargs.argv[]){
-
-//}
