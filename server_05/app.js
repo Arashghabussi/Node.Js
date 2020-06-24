99da86e141f18c99dff6728a904b170e919263bd
arashghabussi <>
2020-06-24 12:47:46 -0700

    yargs.command

---
+++ server_05/app.js
@@ -0,0 +1,49 @@
+const yargs = require('yargs');
+const chalk = require('chalk');
+
+yargs.version('1.1.0');
+
+// To Do List
+
+// Adding:
+yargs.command({
+    command: 'add',
+    describe: 'add a new task',
+    handler: function() {
+        console.log('task added');
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove an existing task',
+    handler: function() {
+        console.log('task removed');
+    }
+});
+
+yargs.command({
+    command: 'edit',
+    describe: 'edit an existing task',
+    handler: function() {
+        console.log('task edited');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'list of tasks',
+    handler: function() {
+        console.log('tasks');
+    }
+});
+
+yargs.command({
+    command: 'history',
+    describe: 'history of tasks',
+    handler: function() {
+        console.log('history of tasks');
+    }
+});
+
+console.log(yargs.argv);
